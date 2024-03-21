8cb4fa45edc95477f14141123d9018183658408c
VhBuriti <victorhugoburit@hotmail.com>
2024-03-21 15:51:35 -0300

    chore: studying about basic web servers

---
+++ webServer/webServer.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const fs = require('fs');
+
+
+
+
+http.createServer((req, res) => {
+
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    let html = fs.readFileSync(__dirname + '/index.html', 'utf8');
+    let message = 'Hello World from server!';
+    html = html.replace('{Message}', message);
+    res.end(html);
+
+}).listen(1337, '127.0.0.1')
